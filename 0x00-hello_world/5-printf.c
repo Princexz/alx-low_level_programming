84ebd85f6d521d7464c16e0d1a87ad3dd6f35940
princexz <prince_solomon@yahoo.com>
2022-12-09 00:52:26 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print a C program using printf
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
