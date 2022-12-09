16c76d166f7d23654404ea390f44d51e59b5dcb1
princexz <prince_solomon@yahoo.com>
2022-12-08 23:44:44 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main -print a line of code using puts
- *
- * return (0)
- */
+ * main- Entry
+ * return: always 0
+ **/
 int main(void)
 {
-	puts('\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return ('0');
 }
