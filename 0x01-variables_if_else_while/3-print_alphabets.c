659cbdf4a513c605edc9fa2d327f17275da4e9e1
princexz <prince_solomon@yahoo.com>
2022-12-10 16:18:42 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,7 +12,7 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
-	for (letter = 'A'; letter <= 'Z', letter++)
+	for (letter = 'A'; letter <= 'Z'; letter++)
 		putchar(letter);
 
 	putchar('\n');
