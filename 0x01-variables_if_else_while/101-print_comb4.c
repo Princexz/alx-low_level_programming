120259b26cbe5a29ba3a4407b93879ca1920f9dc
princexz <prince_solomon@yahoo.com>
2022-12-10 18:07:21 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Prints all possible combinations of three different digits,
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int digit1, digit2, digit3;
+
+	for (digit1 = 0; digit1 < 8; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			{
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
