2a83258a55ae2255dd307114ceca50db420383fb
princexz <prince_solomon@yahoo.com>
2022-12-10 18:58:59 -0800

    My first commit in c programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,8 @@
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora kopar, 2015-10-19\n",59);
+	write(2,
+			"and that piece of art is useful\" - Dora kopar, 2015-10-19\n",
+			59);
 	return (1);
 }
