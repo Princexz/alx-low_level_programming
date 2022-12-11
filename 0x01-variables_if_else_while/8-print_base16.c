ca1815c905f19cdaa11038cf22ecc5f0f4a55aea
princexz <prince_solomon@yahoo.com>
2022-12-10 17:40:37 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int main(void)
 	int num;
 	char letter;
 
-	for (num = 0; num < 10, num++)
+	for (num = 0; num < 10; num++)
 		putchar((num % 10) + '0');
 
 	for (letter = 'a'; letter <= 'f'; letter++)
