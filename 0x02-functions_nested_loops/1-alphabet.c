285c4f0227384dfb26d0b2364353dce14ccd8bbb
princexz <prince_solomon@yahoo.com>
2022-12-14 14:36:36 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,8 +7,8 @@
 void print_alphabet(void)
 {
 	char letter;
-	for (letter = 'a'; letter <= 'z', letter++)
-		_putchar(letter);
-	_putchar('\n');
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
 	return (0);
 }
