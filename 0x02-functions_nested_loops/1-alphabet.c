03d0c7a5490130ef033905ccb56559540d67fc62
princexz <prince_solomon@yahoo.com>
2022-12-14 14:43:05 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,11 @@
 /**
  * main - print the alphabet in lowercase, followed by a new line.
  * Code by Prince Solomon
- * Retuen: 0
  */
 int main(void)
 {
 void print_alphabet(void)
-	char lettere;
+	char letter;
 	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 	putchar('\n');
