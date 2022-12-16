9377d7a756bfe115d24344ecc817b0c1dd1e2b1e
princexz <prince_solomon@yahoo.com>
2022-12-16 12:41:16 -0800

    0x03-debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,12 +11,7 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0
-	
-	/* while (i < 10)*/
-	/* {*/
-	/* putchar(i);*/
-	/* }*/
+	i = 0;
 
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
