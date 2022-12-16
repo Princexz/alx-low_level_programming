4c1bcfade4840e097451342b2493d090debcc1ac
princexz <prince_solomon@yahoo.com>
2022-12-16 12:37:38 -0800

    0x03-debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - causes an i finite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0
+	
+	/* while (i < 10)*/
+	/* {*/
+	/* putchar(i);*/
+	/* }*/
+
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
