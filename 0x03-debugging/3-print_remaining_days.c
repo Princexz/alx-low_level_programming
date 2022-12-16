c677dc361b10b362a5b01bd8f8be2043dd484d97
princexz <prince_solomon@yahoo.com>
2022-12-16 13:25:34 -0800

    0x03-debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,12 +12,13 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-		if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
+		if ((year % 4 == 0 || (year % 400 == 0) && !(year % 100 == 0))
 		{
 			if (month > 2 && day >= 60)
 		{
 			day++;
 		}
+
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
@@ -30,5 +31,6 @@ void print_remaining_days(int month, int day, int year)
 			else
 				printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
+		}
 		}
 }
