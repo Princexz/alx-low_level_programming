79ab5793358ec849065d1e0918685ea39e3d3e18
princexz <prince_solomon@yahoo.com>
2022-12-16 19:13:46 -0800

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square of size sizes
+ * @size: size of the shape
+ * Prince Solomon
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j,
+
+	    if (size <= 0)
+		    _putchar('\n');
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size); j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
