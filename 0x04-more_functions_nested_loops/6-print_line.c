6a66e4aa2837a61f6d79d65672225dfe993a6abc
princexz <prince_solomon@yahoo.com>
2022-12-16 18:59:13 -0800

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_numbers - printing lines
+ * print_line - printing lines
  * @n: numbers of times
  * Program by prince and code by CyberTech
  * Return: void
  */
 
-void print_numbers(int n)
+void print_line(int n)
 {
 	int i;
 
