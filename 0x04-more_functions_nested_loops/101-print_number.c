99d844705c593b3f37cd53adad337d7b169bf6ee
princexz <prince_solomon@yahoo.com>
2022-12-16 20:54:58 -0800

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#onclude "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: .input integer parameter
+ *
+ * code by prince solomon
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -1;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
