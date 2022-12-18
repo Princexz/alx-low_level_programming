1400f31c2d30b5d8dff59062b4ff1bb912519068
princexz <prince_solomon@yahoo.com>
2022-12-17 19:28:32 -0800

    functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints 10 times the alphabet, in lowercases
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercases
  * Code by Prince Solomon
 */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
