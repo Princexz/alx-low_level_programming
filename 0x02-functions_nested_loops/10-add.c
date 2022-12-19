f1af4d5c199654cef24856fa7b4dc46f779b9c97
princexz <prince_solomon@yahoo.com>
2022-12-18 19:46:47 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers.
+ * @num1: the first integer to be added
+ * @num2: the second integer to be added
+ * Code by: Princexz
+ * Return: the result of the addition.
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from input to 98,
+ *		in order seperated by comma followed by a space.
+ * @n: the number to begin counting at.
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			prinf("%d, ", n--);
+		prinf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
