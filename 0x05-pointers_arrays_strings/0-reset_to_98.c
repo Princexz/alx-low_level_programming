7627f041c89e5217466b6f23735ec00b550bdca2
princexz <prince_solomon@yahoo.com>
2022-12-19 20:33:07 -0800

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * reset_t0_98 - takes the pointer and updates the value
+ * reset_to_98 - takes the pointer and updates the value
  * @n: int to check
  * #prince Solomon
  * Return: 0
  */
 
-void reset_t0_98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random valid passwords for the
+ *        program 101-crackme.
+ *
+ * Return: Always 0.
+ */
+int main(void)
