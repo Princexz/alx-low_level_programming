2c1786857fead938396d899b81ecaee8851ec0b6
princexz <prince_solomon@yahoo.com>
2022-12-19 20:44:18 -0800

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- *  * _atoi - Convert a string to an integer.
- *   * @s: The pointer to convert
- *    *
- *     * Return: A integer
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
+ *
+ * Return: A integer
  */
 int _atoi(char *s)
 {
